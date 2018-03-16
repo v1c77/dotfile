a8c546a74ec83b83c59d585faddaedc5c0b1e171
v1c77 <heyuhuade@gmail.com>
2018-03-16 15:08:00 +0800

    centos init scrpit

--- install-in-centos.sh
+++ install-in-centos.sh
@@ -23,66 +23,67 @@ fi
 # git checkout origin/centos-7-version
 
 # install yum depend
-#sudo -S bash -c "wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo &&
-#    yum makecache &&\
-#    yum install -y libxslt-devel libxml2-devel lapack-devel blas-devel \
-#    gcc-fortran swig libssl-dev mysql-devel redis python-devel ruby gcc \
-#    g++ make automake autoconf curl-devel openssl-devel zlib-devel \
-#    httpd-devel apr-devel apr-util-devel sqlite-devel ruby-rdoc ruby-devel \
-#    rubygems perl-ExtUtils-CBuilder perl-ExtUtils-MakeMaker libjpeg-devel  \
-#    devtoolset-3-toolchain centos-release-scl ncurses-devel gcc-c++\
-#    python-pip htop"
-#
+sudo -S bash -c "wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo &&
+    yum makecache &&\
+    yum install -y libxslt-devel libxml2-devel lapack-devel blas-devel \
+    gcc-fortran swig libssl-dev mysql-devel redis python-devel ruby gcc \
+    g++ make automake autoconf curl-devel openssl-devel zlib-devel \
+    httpd-devel apr-devel apr-util-devel sqlite-devel ruby-rdoc ruby-devel \
+    rubygems perl-ExtUtils-CBuilder perl-ExtUtils-MakeMaker libjpeg-devel  \
+    devtoolset-3-toolchain centos-release-scl ncurses-devel gcc-c++\
+    python-pip htop"
+
 
 # install python dependent
-#sudo -S bash -c "pip install -i http://mirrors.aliyun.com/pypi/simple/ \
-#    --trusted-host mirrors.aliyun.com --upgrade pip && \
-#    pip install -i http://mirrors.aliyun.com/pypi/simple/ \
-#    --trusted-host mirrors.aliyun.com \
-#    python-config \
-#    requests \
-#    ipython==5.5.0"
+sudo -S bash -c "pip install -i http://mirrors.aliyun.com/pypi/simple/ \
+    --trusted-host mirrors.aliyun.com --upgrade pip && \
+    pip install -i http://mirrors.aliyun.com/pypi/simple/ \
+    --trusted-host mirrors.aliyun.com \
+    python-config \
+    requests \
+    ipython==5.5.0"
 
 
 # upgrade vim
-#if pushd ${work_dir}; then
-#    sudo -S bash -c "curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo && \
-#        yum upgrade -y || true &&\
-#        yum update vim* -y || echo 'vim update complete'"
-#    vim --version
-#    popd
-#fi
+if pushd ${work_dir}; then
+    sudo -S bash -c "curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo && \
+        yum upgrade -y || true &&\
+        yum update vim* -y || echo 'vim update complete'"
+    vim --version
+    popd
+fi
 
 # install vim plugin manager
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-# https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install zsh & oh-my-zsh
 
-# if pushd ${home_dir}; then
-#     sudo -S yum install -y zsh
-#     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || echo 'install omz complete'
-#     chsh -s /bin/zsh
-#     popd
-# fi
+ if pushd ${home_dir}; then
+     sudo -S yum install -y zsh
+     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || echo 'install omz complete'
+     chsh -s /bin/zsh
+     popd
+ fi
 
 # insstall golang
-#if pushd ${work_dir}; then
-#    curl -LO https://dl.google.com/go/go1.10.linux-amd64.tar.gz
-#    sudo -S tar -C /usr/local -xvzf go1.10.linux-amd64.tar.gz
-#    mkdir -p ~/go_home/{bin,pkg,src}
-#    echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile.d/path.sh
-#    echo 'export GOBIN="$HOME/go_home/bin"' | tee -a ~/.zshrc
-#    echo 'export GOPATH="$HOME/go_home/src"' | tee -a ~/.zshrc
-#    source /etc/profile && source ~/.zshrc
-#    popd
-#fi
-#
+if pushd ${work_dir}; then
+    curl -LO https://dl.google.com/go/go1.10.linux-amd64.tar.gz
+    sudo -S tar -C /usr/local -xvzf go1.10.linux-amd64.tar.gz
+    mkdir -p ~/go_home/{bin,pkg,src}
+    echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile.d/path.sh
+    echo 'export GOBIN="$HOME/go_home/bin"' | tee -a ~/.zshrc
+    echo 'export GOPATH="$HOME/go_home/src"' | tee -a ~/.zshrc
+    source /etc/profile && source ~/.zshrc
+    popd
+fi
+
 # install hub
+sudo -S gem install bundler
 if pushd ${work_dir}; then
     git clone https://github.com/github/hub.git || true
     cd hub
-    make install prefix=/usr/local
+    sudo -S make install prefix=/usr/local
     popd
 fi
 
